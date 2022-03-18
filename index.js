1be55622645b9abc52aef91d607289c53bb770a0
Chugus <chugus808106@gmail.com>
2022-03-17 23:36:55 -0600

    Avances

--- index.js
+++ index.js
@@ -13,21 +13,24 @@ dbConnection();
 app.use(cors())
 
 // Directorio Público
-app.use( express.static('public') );
+app.use(express.static('public'));
 
 // Lectura y parseo del body
-app.use( express.json() );
+app.use(express.json());
 
 // Rutas
-app.use('/api/auth', require('./routes/auth') );
-app.use('/api/events', require('./routes/events') );
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../public", "index.html"));
+});
 
 
 
 
 // Escuchar peticiones
-app.listen( process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
 
 
